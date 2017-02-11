ffcdf7fa26ea21d1f30fa54c0732354fc80d06e1
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-02-11 17:19:25 -0500

    Consolidating coverage reporting

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -2,12 +2,7 @@
 ##   This file was generated via the commitment:install generator. You are free
 ##   and expected to change this file.
 if ENV['COV'] || ENV['COVERAGE'] || ENV['TRAVIS']
-  if ENV['TRAVIS']
-    require 'simplecov'
-    require "codeclimate-test-reporter"
-    SimpleCov.start
-  elsif ENV['COV'] || ENV['COVERAGE']
-    require 'simplecov'
-    SimpleCov.start
-  end
+  require "codeclimate-test-reporter" if ENV['TRAVIS']
+  require 'simplecov'
+  SimpleCov.start
 end
